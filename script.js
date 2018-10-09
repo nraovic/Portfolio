cf3d20b75c1d334b824655009c4dd930f88ee777
nraovic <n.raovic@gmail.com>
2018-10-09 16:04:15 +0200

    Create modal for each project

--- script.js
+++ script.js
@@ -10,6 +10,48 @@ const projectsData = [
   { title: 'Portfolio Site', githubLink: '', website: '' }
 ];
 
+const container = document.querySelector('.container');
+
+const modal = document.querySelector('.modal');
+
+const createModal = project => {
+  const modal = document.createElement('div');
+  modal.className = 'project-modal';
+  const closeModal = document.createElement('a');
+  closeModal.className = 'close-modal';
+  const closeSign = document.createElement('i');
+  closeSign.classList.add('fas', 'fa-times');
+  closeModal.appendChild(closeSign);
+  const title = document.createElement('h3');
+  title.textContent = project.title;
+  const description = document.createElement('p');
+  description.textContent = project.description;
+  const website = document.createElement('p');
+  const projectLink = document.createElement('a');
+  projectLink.href = project.website;
+  website.appendChild(projectLink);
+  const github = document.createElement('p');
+  const githubLink = document.createElement('a');
+  github.appendChild(githubLink);
+
+  modal.appendChild(closeModal);
+  modal.appendChild(title);
+  modal.appendChild(description);
+  modal.appendChild(website);
+  modal.appendChild(github);
+
+  document.body.insertBefore(modal, container);
+};
+//
+// Open Modal
+const openModal = modal => {
+  modal.classList.add('open');
+};
+
+//Close Modal
+const closeModal = modal => {
+  modal.classList.remove('open');
+};
 // Create Project Element
 const projectsList = document.querySelector('.projects-list');
 
@@ -23,4 +65,24 @@ for (let project of projectsData) {
   link.appendChild(h2);
   li.appendChild(link);
   projectsList.appendChild(li);
+
+  createModal(project);
+
+  const modal = document.querySelector('.project-modal');
+  const close = document.querySelector('.close-modal');
+  const header = document.querySelector('.header');
+  const main = document.querySelector('.main');
+  const footer = document.querySelector('.footer');
+  link.onclick = () => {
+    openModal(modal);
+    for (let i of [header, main, footer]) {
+      i.classList.add('hide');
+    }
+  };
+  close.onclick = () => {
+    closeModal(modal);
+    for (let i of [header, main, footer]) {
+      i.classList.remove('hide');
+    }
+  };
 }
