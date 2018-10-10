5552ac7a685839f6c4babed6518cf2c03c5acb1a
nraovic <n.raovic@gmail.com>
2018-10-10 14:14:34 +0200

    Add additional shapes with larger sizes to the background

--- shapes.js
+++ shapes.js
@@ -1,13 +1,17 @@
-const listOfShapes = ['◯', '◦', '▶', '△', '◗'];
+const listOfShapes = [, '◦', '▶', '◯', '◗'];
+const listOfBigShapes = ['△', '◻', '◯', '☓'];
 
-const numberofShapes = 10;
+const numberofShapes = 7;
+const numberofBigShapes = 2;
 
+const scale = 6;
+const bigShapesScale = 18;
 for (let shape of listOfShapes) {
 }
 
-function getRandomPosition(element) {
-  var x = document.body.offsetHeight - element.clientHeight * 6;
-  var y = document.body.offsetWidth - element.clientWidth * 6;
+function getRandomPosition(element, scale) {
+  var x = document.body.offsetHeight - element.clientHeight * scale;
+  var y = document.body.offsetWidth - element.clientWidth * scale;
   var randomX = Math.floor(Math.random() * x);
   var randomY = Math.floor(Math.random() * y);
   return [randomX, randomY];
@@ -24,12 +28,29 @@ window.onload = function() {
       shape1.setAttribute('style', 'position:absolute;');
       shape1.textContent = shape;
       document.body.appendChild(shape1);
-      var xy = getRandomPosition(shape1);
+      var xy = getRandomPosition(shape1, 6);
       shape1.style.top = xy[0] + 'px';
       shape1.style.left = xy[1] + 'px';
       shape1.style.zIndex = -1;
       shape1.style.opacity = 0.4;
       shape1.style.color = '#D3D3D3';
+      i++;
+    }
+  }
+  for (let shape of listOfBigShapes) {
+    let i = 0;
+    while (i < numberofBigShapes) {
+      const bigShape = document.createElement('div');
+      bigShape.className = 'big-shape';
+      bigShape.setAttribute('style', 'position:absolute;');
+      bigShape.textContent = shape;
+      document.body.appendChild(bigShape);
+      var xy = getRandomPosition(bigShape, 18);
+      bigShape.style.top = xy[0] + 'px';
+      bigShape.style.left = xy[1] + 'px';
+      bigShape.style.zIndex = -1;
+      bigShape.style.opacity = 0.4;
+      bigShape.style.color = '#8f9779';
       i++;
     }
   }
