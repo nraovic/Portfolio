b62ca7489ca3e5e21ecaba931f70583a41bf7799
nraovic <n.raovic@gmail.com>
2018-11-05 12:30:36 +0100

    Enable blurry icons on hover only for large screen size

--- blurryIcons.js
+++ blurryIcons.js
@@ -1,17 +1,19 @@
 const iconsList = document.querySelectorAll('.contact-icon');
 const icons = Array.from(iconsList);
 
-for (let icon of icons) {
-  icon.addEventListener('mouseenter', () => {
-    for (let element of icons) {
-      if (element !== icon) {
-        element.style.textShadow = '0 0 5px #000';
+if (document.documentElement.clientWidth >= 1024) {
+  for (let icon of icons) {
+    icon.addEventListener('mouseover', () => {
+      for (let element of icons) {
+        if (element !== icon) {
+          element.style.textShadow = '0 0 5px #000';
+        }
       }
-    }
-  });
-  icon.addEventListener('mouseleave', () => {
-    for (let element of icons) {
-      element.style.textShadow = 'none';
-    }
-  });
+    });
+    icon.addEventListener('mouseout', () => {
+      for (let element of icons) {
+        element.style.textShadow = 'none';
+      }
+    });
+  }
 }
